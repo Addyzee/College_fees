eabfb3af19d97207238115cd64b8a3e4be9c4331
mbeka02 <93541073+mbeka02@users.noreply.github.com>
2022-04-01 12:00:06 +0300

    Update Tables.sql

--- Tables.sql
+++ Tables.sql
@@ -102,3 +102,9 @@ ADD CONSTRAINT df_Sponsor
 DEFAULT 'Self' FOR Sponsor;
 
 --Default Sponsor is 'Self'
+
+create view v_student_sponsors
+AS
+SELECT Reg_no, Student_name, Contact, Sponsor
+FROM Student_details;
+
