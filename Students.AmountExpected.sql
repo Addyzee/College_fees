825d1eac5b29b2faa663b611e7ea74dbed079f3b
Addyzee <andrewkamaukim@gmail.com>
2022-04-05 10:55:47 +0300

    work on student expect

--- Students.AmountExpected.sql
+++ Students.AmountExpected.sql
@@ -1,4 +1,12 @@
-SELECT std.Reg_no, std.Student_name, std.ProgrammeID, std.No_of_courses, pg.Cost_per_course,No_of_courses*pg.Cost_per_course AS Amount_expected
+SELECT 
+--std.Reg_no,
+--std.Student_name, 
+std.ProgrammeID AS Programme_ID, --std.No_of_courses, 
+--pg.Cost_per_course,No_of_courses*pg.Cost_per_course AS Amount_expected,
+SUM(std.No_of_courses*pg.Cost_per_course) AS Prog_total
 FROM Student_details std
 JOIN Programmes pg
-ON pg.ProgrammeID=std.ProgrammeID;
+ON pg.ProgrammeID=std.ProgrammeID
+WHERE pg.ProgrammeID IN 'P15'
+GROUP BY pg.ProgrammeID;
+--GROUP BY pg.ProgrammeID, Reg_no, Cost_per_course,No_of_courses*pg.Cost_per_course, std.No_of_courses, std.ProgrammeID,std.Student_name;
